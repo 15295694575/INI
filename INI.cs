87cf2910eee5a75e4142c5632828fc54ce5f184b
15295694575 <46291496+15295694575@users.noreply.github.com>
2021-08-16 10:07:38 +0800

    Add files via upload

---
+++ INI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniOperation
+{
+    public class INI
+    {
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key,string def,StringBuilder retval,int size, string filePath);
+
+        public static string GetIniValue(string filePath,string section,string key,int size=1024)
+        {
+            StringBuilder sb = new StringBuilder();
+            GetPrivateProfileString(section, key, "", sb, size, filePath);
+            return sb.ToString();
+        }
+
+        public static void WriteIniValue(string filePath,string section,string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, filePath);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\yinyj\Desktop\1.ini";
+            string value = INI.GetIniValue(filePath, "Main", "Author");
+            Console.WriteLine(value);
+            INI.WriteIniValue(filePath, "Main", "CreateDatetime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            Console.ReadKey();
+        }
+    }
+}
