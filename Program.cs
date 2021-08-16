584529b7af022494a2b4f1c5a5577f52bd1bb70c
Yinyj <yinyj@chbiomedical.com>
2021-08-16 10:25:55 +0800

    Update ini path

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace IniOperation
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\yinyj\Desktop\1.ini";
+            string filePath =Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"1.ini");
             string value = INI.GetIniValue(filePath, "Main", "Author");
             Console.WriteLine(value);
             INI.WriteIniValue(filePath, "Main", "CreateDatetime", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
